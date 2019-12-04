15b782832a3cc98a2f27ef567749a36a930f4709
Dean Agan <agandfr@gmail.com>
2019-12-04 15:09:07 +1100

    Add entry service and components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { EntriesComponent } from './entries/entries.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+
+
+// services
+import { EntryService } from './entry.service';
+import { AppRouterModule } from './app.router.module';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EntriesComponent,
+    FooterComponent,
+    HeaderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    AppRouterModule
+
   ],
-  providers: [],
+  providers: [EntryService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/entry.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+//import { HttpClient } from '@selenium-webdriver/http';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EntryService {
+
+  baseUrl: string = 'http://localhost:57532/api/entries'
+
+  constructor(private http: HttpClient) { }
+
+  getAll(){
+    return this.http.get(this.baseUrl);
+  }
+}
